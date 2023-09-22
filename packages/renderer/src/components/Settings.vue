3824b5a3c493c5a760f7a4ae9390ce9525a6772e
erguotou <erguotou525@gmail.com>
2023-09-22 08:21:31 +0800

    chore: add css

---
+++ packages/renderer/src/components/Settings.vue
@@ -0,0 +1,13 @@
+<template>
+  <footer class="footer">
+    <div class="flex items-center">
+      <input
+        type="checkbox"
+        class="checkbox"
+      />
+      <label for="checkbox">自动生成webp图片</label>
+    </div>
+    <div class="flex items-center ml-4">
+    </div>
+  </footer>
+</template>
--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 import {renderer} from 'unplugin-auto-expose';
 import {join} from 'node:path';
 import {injectAppVersion} from '../../version/inject-app-version-plugin.mjs';
+import UnoCSS from 'unocss/vite';
 
 const PACKAGE_ROOT = __dirname;
 const PROJECT_ROOT = join(PACKAGE_ROOT, '../..');
@@ -48,6 +49,7 @@ const config = {
       preloadEntry: join(PACKAGE_ROOT, '../preload/src/index.ts'),
     }),
     injectAppVersion(),
+    UnoCSS(),
   ],
 };
 
